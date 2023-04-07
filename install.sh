2e11c701be3157771ca93da5d562b94da5ba1428
amir <amir.h.rassafi@gmail.com>
2023-04-07 10:37:31 +0330

    Add nginx

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt install docker.io docker-compose -y
-sudo apt install python3-certbot-nginx -y
+sudo apt install nginx python3-certbot-nginx -y
 docker-compose -f docker-compose-xui.yml up -d
