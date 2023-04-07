f84f62c053132313f8cfb819e36d0315e01abcfd
amir <amir.h.rassafi@gmail.com>
2023-04-07 10:44:15 +0330

    Better

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
+echo 'Install dependencies'
 sudo apt update
 sudo apt install docker.io docker-compose -y
 sudo apt install nginx python3-certbot-nginx -y
 docker-compose -f docker-compose-xui.yml up -d
+
+ip=$(curl ifconfig.io)
+echo 'Your Ip is: $ip, please set domain for this IP' 
