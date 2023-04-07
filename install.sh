741b854a1ca126c3bcff347ba53ac58e245a9f04
amir <amir.h.rassafi@gmail.com>
2023-04-07 11:32:21 +0330

    Let check it

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ sudo disable ufw
 ip=$(curl -s ifconfig.io)
 echo "Your Ip is:  $ip , please give me domain:"
 read domain
+certbot --nginx -d $domain
 cp ./nginx.conf default
 sed -i "s/{{domain}}/$domain/g" default
 cp default /etc/nginx/sites-enabled/default
-certbot --nginx -d $domain
 sudo service nginx restart
 echo "Now check your domain!!SSL signing done!"
