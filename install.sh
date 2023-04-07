b0d5095289fc8878e59145e2836c6d0041cf8bab
amir <amir.h.rassafi@gmail.com>
2023-04-07 10:34:49 +0330

    Add yes switch

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt install docker.io docker-compose -y
-sudo apt install python3-certbot-nginx
+sudo apt install python3-certbot-nginx -y
 docker-compose -f docker-compose-xui.yml up -d
