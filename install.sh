58421dc60af9061f77c9c41d7ab33f1d3fbc1805
amir <amir.h.rassafi@gmail.com>
2024-07-17 08:33:56 +0100

    Update installer.

--- install.sh
+++ install.sh
@@ -2,4 +2,9 @@ echo "Install dependencies"
 sudo apt update
 sudo apt install stunnel4 -y
 sudo apt install openssl -y
-sudo bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+sudo apt install btop -y
+sudo apt install net-tools -y
+curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh > v2ray-xui.sh
+sudo chmod +x v2ray-xui.sh
+sudo ./v2ray-xui.sh
+
